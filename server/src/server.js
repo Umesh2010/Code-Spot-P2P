0e79189d7acc400a0d2207cc693c38c99d33c54f
Umesh2010 <umesh.panchal501@gmail.com>
2022-10-11 22:53:14 +0530

    test 1

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,9 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config/config.env" });
 const port = process.env.PORT || 3000;
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(require("./routes/values"));
